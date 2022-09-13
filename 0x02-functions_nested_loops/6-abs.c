2640a272d4c3aabd55f7083cc62a35ca09ad7562
Emmy-Iraturinze <iraturinzeemmanuel@gmail.com>
2022-09-13 00:52:52 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer
+ * @n: number being tested
+ * Return: return with conditions
+ */
+
+int _abs(int n)
+{
+if (n < 0)
+
+return (n * -1);
+
+								}
+return (n);
+}
+
