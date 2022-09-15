9760d408cfdabd1171ffe9ab01c7eaab072c20c5
Emmy-Iraturinze <iraturinzeemmanuel@gmail.com>
2022-09-15 04:58:52 -0700

    0-isupper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/*
+ *main -header for porototypes
+ *void prototypes(void)
+ *int prototypes(int)
+ */
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -13,6 +19,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-/*
- * header file for functions*/
+
 #endif
